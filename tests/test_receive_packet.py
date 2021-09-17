fdd38327fb34d9474ced61ebca9e202a25bae03c
Song Yi Wei <ywsong@aceinna.com>
2021-09-17 14:26:06 +0800

    fix: update test case

--- tests/test_receive_packet.py
+++ tests/test_receive_packet.py
@@ -37,7 +37,7 @@ LOCAL_MAC_ADDRESS = app_conf['local']['mac']
 RESOLVED_LOCAL_IFACE = resolve_iface(LOCAL_IFACE)
 
 
-def create_device(name, device_mac_address):
+def create_device(name, device_mac_address, sn):
     current_time = time.localtime()
     dir_time = time.strftime(
         "%Y%m%d_%H%M%S", current_time)
@@ -55,7 +55,7 @@ def create_device(name, device_mac_address):
         'imu': 'device imu',
         'pn': 'part number',
         "firmware_version": '1.0.0',
-        'sn': '123456'
+        'sn': sn
     }
 
     app_info = {
@@ -85,14 +85,37 @@ def create_mock_devices(name_prefix, device_mac_address):
     devices = []
     for mac_address in device_mac_address:
         sn = convert_mac_to_sn(mac_address)
-        print('{0}:{1}'.format(sn, mac_address))
+        print('{0} - {1}'.format(sn, mac_address))
         devices.append(
             create_device(
                 '{0}-{1}'.format(name_prefix, sn),
-                mac_address
+                mac_address,
+                sn
             )
         )
     return devices
+
+
+def calc_packet_count(second, step=10):
+    packet_count = 0
+
+    frequency_policy = {
+        IMU_PKT: 1,
+        GNSS_PKT: 100,
+        INS_PKT: 1,
+        ODO_PKT: 10,
+        DIAG_PKT: 100,
+        RTCM_PKT: 1,
+        'nmea': 100
+    }
+
+    total_steps = int(second/step)
+    for i in range(total_steps):
+        for key in frequency_policy:
+            if i % frequency_policy[key] == 0:
+                packet_count += 1
+
+    return packet_count
 
 
 def gen_mock_packet():
@@ -108,7 +131,7 @@ def gen_mock_packet():
     }
 
     mock_nmea_bytes = '$GPGGA,051612.00,3129.6798000,N,12021.8016833,E,1,33,0.0,-15.409,M,6.809,M,0.0,*6E\r\n$GPZDA,051612.00,15,09,2021,00,00,*47'.encode()
-    empty_bytes = bytes(200)
+    empty_bytes = bytes(300)
 
     while True:
         output_packet = {}
@@ -145,6 +168,27 @@ def send_packet_to_devices(device_mac_addresses, packet):
     return packet_list
 
 
+def gen_packet_to_devices(device_mac_addresses, packet):
+    packet_list = PacketList()
+    for mac_address in device_mac_addresses:
+        for packet_type in packet:
+            actual_packet_type = packet_type
+
+            if packet_type == 'nmea':
+                actual_packet_type = None
+
+            command_line = message.build(
+                dst_mac=LOCAL_MAC_ADDRESS,
+                src_mac=mac_address,
+                pkt=actual_packet_type,
+                payload=packet[packet_type])
+            packet_raw = Packet(command_line)
+            packet_list.append(packet_raw)
+
+    #sendp(packet_list, iface=RESOLVED_LOCAL_IFACE, verbose=0)
+    return packet_list
+
+
 def format_log_info(devices):
     for device in devices:
         device.update_received_packet_info()
@@ -153,45 +197,57 @@ def format_log_info(devices):
 
 
 def create_devices_process(mock_mac_addresses, status_flag):
+    app_logger.new_session()
+
     devices = create_mock_devices('mock-prefix', mock_mac_addresses)
     for device in devices:
         device.start()
 
     while True:
-        if status_flag.value == 1:
+        # if status_flag.value == 1:
+        #    print('Mock packet sent.')
+        #    break
+        if devices[0]._received_packet_info[IMU_PKT] >= 1000:
+            print('[Device] Receive end: {0}'.format(time.time()))
             break
-        time.sleep(0.01)
+        time.sleep(1)
 
     format_log_info(devices)
 
 
 def gen_output_process(mock_mac_addresses, status_flag):
-    output_packet_count = 0
-    start_time = time.time()
-    now_time = start_time
+    duration = 10  # minute as unit
 
     mock_packet_generator = gen_mock_packet()
-    print('Start time:{0}'.format(start_time))
-    while now_time-start_time < duration*10:
+
+    plan_gen_count = duration*calc_packet_count(1000)*len(mock_mac_addresses)
+    plan_send_packet_list = []
+    print('[Data Mocker] Plan mock packet count:', plan_gen_count)
+    print('[Data Mocker] Prepare start: {0}'.format(time.time()))
+    while plan_gen_count > 0:
         packet = next(mock_packet_generator)
-        # send same packet
-        ethernet_packet_list = send_packet_to_devices(
+        ethernet_packet_list = gen_packet_to_devices(
             mock_mac_addresses, packet)
-        output_packet_count += len(ethernet_packet_list)
-        now_time = time.time()
-    print('End time:{0}'.format(now_time))
-    print('Output packet count:{0}'.format(output_packet_count))
+
+        plan_gen_count -= len(ethernet_packet_list)
+        plan_send_packet_list.append(ethernet_packet_list)
+    print('[Data Mocker] Prepare end: {0}'.format(time.time()))
+
+    print('[Data Mocker] Start send time: {0}'.format(time.time()))
+    for batch_packet_list in plan_send_packet_list:
+        sendp(batch_packet_list, iface=RESOLVED_LOCAL_IFACE, verbose=0)
+        time.sleep(0.01)
+    print('[Data Mocker] End send time: {0}'.format(time.time()))
+    #print('Output packet count:{0}'.format(output_packet_count))
 
     status_flag.value = 1
 
 
 if __name__ == '__main__':
-    app_logger.new_session()
-
     # 1. create shared parameters
     output_packet_count = 0
-    mock_len = 10
-    duration = 1  # minute as unit
+    mock_len = 15
+
     mock_mac_addresses = gen_mock_mac_addresses(mock_len)
     status_flag = Value('i', 0)
     # 2. create devices as receiver
