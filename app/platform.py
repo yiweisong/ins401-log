932e3f0f7e17b692ce2f6673d88ddf9a43c1b024
Song Yi Wei <ywsong@aceinna.com>
2022-05-09 15:02:50 +0800

    fix: resolve cannot exit with ctrl+c

--- app/platform.py
+++ app/platform.py
@@ -1,5 +1,6 @@
 class win:
     def disable_console_quick_edit_mode():
-        from ctypes import windll
-        kernel32 = windll.kernel32
-        kernel32.SetConsoleMode(kernel32.GetStdHandle(-10), 128)
+        from .win_types import (update_console_mode, ENABLE_EXTENDED_FLAGS, ENABLE_QUICK_EDIT_MODE)
+        mask = ENABLE_EXTENDED_FLAGS | ENABLE_QUICK_EDIT_MODE
+        flags = ENABLE_EXTENDED_FLAGS
+        update_console_mode(flags, mask, restore=True)
